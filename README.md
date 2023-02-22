cb320d91514212cf890a4b0f05b531c55dd8d07b
Syrinthia Swift <116780275+sswift1995@users.noreply.github.com>
2023-02-21 21:03:40 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # swiftySnakes-P1PG
+ This is my Browser Game Project 1.
+how to use var variables:
+ https://study.com/academy/lesson/variable-basics-in-javascript-use-examples.html?src=ppc_adwords_nonbrand&rcntxt=aws&crt=646616438141&kwd=&kwid=aud-341678082384:dsa-1253079156202&agid=125582019081&mt=&device=c&network=g&_campaign=SeoPPC-desktop&gclid=CjwKCAiA9NGfBhBvEiwAq5vSy6UuXU_36vODKRKMDboCvkL68yScmlzvRdKRHgPvYGhDG4uG1QVf5BoCw-wQAvD_BwE
--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var rows = 25;
 var cols = 25;
 var board;
 var context;
-
+var drawScore;
 
 //Snake head
 var snakeyX = blockSize * 3;
@@ -13,11 +13,18 @@ var snakeyY = blockSize * 3;
 var velocityX = 0;
 var velocityY = 0;
 
+var snakeBody = [];
+
+
+var score = 0;
+
 //bait
 var munchyX; 
 var munchyY;
 
+var youLose = false;
 
+//Calling the movement 
 window.onload = function() {
     board = document.getElementById("board");
     board.height = rows * blockSize;
@@ -26,44 +33,96 @@ window.onload = function() {
     
     placeFood();
     document.addEventListener("keyup", changeDirection);
+    
     //update();
     setInterval(update,1000/10); //100 millisec
 }
 
-function update(){
+//Operating Objects
+
+function update() {
+
+if (youLose){
+    return;
+}
+
     context.fillStyle="black";
     context.fillRect(0,0, board.width, board.height);
 
-    context.fillStyle= "teal";
-    snakeyX += velocityX;
-    snakeyY += velocityY;
-    context.fillRect(snakeyX, snakeyY, blockSize,blockSize);
-
     context.fillStyle="pink";
     context.fillRect(munchyX, munchyY, blockSize, blockSize);
+
+if (snakeyX == munchyX && snakeyY == munchyY){
+    snakeBody.push([munchyX, munchyY]);
+    placeFood();
+}
+    
+for (let i = snakeBody.length-1; i > 0; i--) {
+    snakeBody[i] = snakeBody[i-1];
+}
+if (snakeBody.length){
+    snakeBody[0] = [snakeyX, snakeyY];
 }
 
+    context.fillStyle= "teal";
+    snakeyX += velocityX * blockSize;
+    snakeyY += velocityY * blockSize; 
+    context.fillRect(snakeyX, snakeyY, blockSize, blockSize);
+    for (let i = 0; i < snakeBody.length; i++) { 
+        context.fillRect(snakeBody[i][0], snakeBody[i][1], blockSize,blockSize);
+        newFunction();
+    }
+
+
+//You lose conditioning
+if (snakeyX < 0 || snakeyX > cols*blockSize || snakeyY < 0 || snakeyY > rows*blockSize) {
+    youLose = true;
+    alert("Game Over");
+}
+
+for (let i = 0; i < snakeBody.length; i++) {
+    if (snakeyX == snakeBody[i][0] && snakeyY == snakeBody[i][1]) {
+        youLose = true;
+        alert("Game Over");
+    }
+}
+
+    function newFunction() {
+        if (snakeyX === munchyX && snakeyY === munchyY) {
+            snakeBody++ +
+                score; 1;
+        }
+    }
+}
+
+function drawScore(){
+    context.fillStyle = "white";
+    context.font = "10px Verdana";
+    context.fillText("Score" + score, board.width-50, 10);
+}
+
+//Function controls Arrows
 function changeDirection(s){
-    if (s.code == "ArrowUp"){
+    if (s.code == "ArrowUp" && velocityY != 1){
         velocityX = 0;
         velocityY = -1;
     }
-    else if (s.code == "ArrowDown"){
+    else if (s.code == "ArrowDown" && velocityY != -1){
         velocityX = 0;
         velocityY = 1;
     }
-    else if (s.code == "ArrowLeft"){
+    else if (s.code == "ArrowLeft" && velocityX != 1){
         velocityX = -1;
         velocityY = 0;
     }
-    else if(s.code == "ArrowRight"){
+    else if(s.code == "ArrowRight" && velocityX != -1){
         velocityX = 1;
         velocityY = 0;
     }
 } 
 
 
-
+ 
 function placeFood() {
     munchyX = Math.floor(Math.random() * cols) * blockSize;
     munchyY = Math.floor(Math.random()* rows) * blockSize;
